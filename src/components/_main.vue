6187e187e14d62c3f870ce16455b5339a9a7a318
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-13 17:02:49 +0200

    create main component

---
+++ src/components/_main.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+
+   name: 'appMain',
+
+   data() {
+      return {
+
+      }
+   }
+
+}
+</script>
+
+<template>
+   <main class="bg-dark">
+      <div class="container">
+         <section id="main_sec">
+            <h3 class="m-0 py-5 text-white">-- Content Goes Here --</h3>
+         </section>
+         <!-- /#main_sec -->
+      </div>
+      <!-- /.container -->
+   </main>
+</template>
+
+
+<style lang="scss" scoped></style>
