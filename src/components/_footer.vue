11577c7eb080f763d01b40415d0a7e746b661a00
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-13 18:11:41 +0200

    create top footer

---
+++ src/components/_footer.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+
+   name: 'appFooter',
+
+}
+
+</script>
+
+<template>
+   <footer>
+      <section id="footer_top">
+         <div class="container d-flex justify-content-between align-items-center py-3">
+            <div id="links" class="d-flex text-white">
+               <div>
+                  <h5>DC COMICS</h5>
+                  <ul class="list-unstyled">
+                     <li><a href="#">Characters</a></li>
+                     <li><a href="#">Comics</a></li>
+                     <li><a href="#">Movies</a></li>
+                     <li><a href="#">TV</a></li>
+                     <li><a href="#">Games</a></li>
+                     <li><a href="#">Videos</a></li>
+                     <li><a href="#">News</a></li>
+                  </ul>
+                  <h5>SHOP</h5>
+                  <ul class="list-unstyled">
+                     <li><a href="#">Shop DC</a></li>
+                     <li><a href="#">Shop DC Collectibles</a></li>
+                  </ul>
+               </div>
+               <div class="mx-5">
+                  <h5>DC</h5>
+                  <ul class="list-unstyled">
+                     <li><a href="#">Terms Of Use</a></li>
+                     <li><a href="#">Privacy policy (New)</a></li>
+                     <li><a href="#">Ad Choices</a></li>
+                     <li><a href="#">Advertising</a></li>
+                     <li><a href="#">Jobs</a></li>
+                     <li><a href="#">Subscriptions</a></li>
+                     <li><a href="#">Talent Workshops</a></li>
+                     <li><a href="#">CPSC Certificates</a></li>
+                     <li><a href="#">Ratings</a></li>
+                     <li><a href="#">Shop Help</a></li>
+                     <li><a href="#">Contact Us</a></li>
+                  </ul>
+               </div>
+               <div>
+                  <h5>SITES</h5>
+                  <ul class="list-unstyled">
+                     <li><a href="#">DC</a></li>
+                     <li><a href="#">MAD Magazine</a></li>
+                     <li><a href="#">DC Kids</a></li>
+                     <li><a href="#">DC Universe</a></li>
+                     <li><a href="#">DC Power Visa</a></li>
+                  </ul>
+               </div>
+            </div>
+            <div class="foot_logo">
+               <img height="350" src="./assets/img/dc-logo-bg.png" alt="bg_dc_logo">
+            </div>
+         </div>
+      </section>
+   </footer>
+</template>
+
+
+<style lang="scss" scoped></style>
