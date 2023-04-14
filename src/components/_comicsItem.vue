8974e2e8d1afd2298315e3bc78ff568221ee5776
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-14 17:28:00 +0200

    create comics itmem

---
+++ src/components/_comicsItem.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+
+   name: 'comicsItem',
+
+   props: {
+      thumb: String,
+      price: String,
+      series: String,
+      type: String,
+   }
+
+}
+</script>
+<template>
+   <div class="col">
+      <div class="item">
+         <img :src="thumb" :alt="series">
+         <div class="desc text-white py-3 text-uppercase">
+            <span>{{ series }}</span>
+         </div>
+      </div>
+   </div>
+</template>
+
+
+<style lang="scss" scoped>
+.item {
+   cursor: pointer;
+}
+
+img {
+   height: 200px;
+   width: 100%;
+   object-fit: cover;
+   object-position: top;
+}
+</style>
